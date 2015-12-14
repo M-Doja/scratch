08ecdc119f498614fa0148573aec07f5800ec86b
M-Doja <micdoja@yahoo.com>
2015-12-14 13:28:08 -0800

    added basic crud

--- src/js/app.js
+++ src/js/app.js
@@ -7,22 +7,47 @@ function addName() {
   if(userName !== '') {
   var newPerson = new person(userName);
   People.push(newPerson);
-  document.getElementById('result').innerHTML += "<h4>" + userName + "</h4>";
   console.log(newPerson.name);
   console.log(People);
   $('#userName').val("");
-  // showEveryBody();
+  showPeople();
   } else {
     alert('Please enter a name.')
   }
 }
+function showPeople() {
+  $("#result").text('');
+var holder = '';
+for (var i = 0; i < People.length; i++){
+  holder += "<ul>";
+  holder += "<li><h3>" + People[i].name + "</h3></li>";
+  holder += "</ul>";
+  holder += "<div id='save" + i +"'><button class='btn btn-sm btn-primary' onclick='deletePerson(" + i + ")'>Delete " + People[i].name +" </button><button class='btn btn-sm btn-primary' onclick='editPerson(" + i + ")'>Edit " + People[i].name +"</button></div>";
+  }
+  $('#result').html(holder);
+}
+// Delete person by their index position
+function deletePerson(index) {
+  // Splice from array that index, remove that one
+  People.splice(index, 1);
+  // Display People array
+  showPeople();
+}
+// Edit person by their index position
+function editPerson(index) {
+  // Save seleted person name as 'editname'
+  var editName = People[index].name;
 
+  document.getElementById('userName').value = editName;
+  $('#editUserName').val(index);
+  var save = '<button onclick="savePerson()"><Save</button>';
+  $('#save' + index).html(save);
+}
+function savePerson(index) {
+  var editName = $("#userName").val();
+  var index = $("#editUserName").val();
+  People[index].name = editName;
+  $('#userName').val("");
+  showPeople();
 
-
-
-
-
-
-// function editName() {
-//   var name = $('#userName').val();
-// }
+}
